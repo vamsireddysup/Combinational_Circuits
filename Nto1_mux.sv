91d8bef2db84af283df08ab94291f56fa82c1db3
vamsireddysup <vamsireddysup@gmail.com>
2025-01-16 19:01:07 -0800

    Commit Check

---
+++ Nto1_mux.sv
@@ -0,0 +1,10 @@
+module Nto1_mux(#parameter N=2)(
+	input bit [N-1:0][3:0]ip,
+	input bit [$clog2(N)-1:0]sel,
+	output bit [3:0]out
+	);
+	x
+
+
+
+endmodule:Nto1_mux
---
+++ check.v
@@ -0,0 +1,4 @@
+module check
+
+
+endmodule:check
